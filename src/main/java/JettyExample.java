6c32905d273c5b6bdff4ac480a91beb75b4fbf38
Carlos Hernán Hoyos Gómez <carlos.gomez88@gmail.com>
2016-05-13 15:34:36 -0500

    funcionalidad para desplegar a heroku

--- src/main/java/JettyExample.java
+++ src/main/java/JettyExample.java
@@ -1,36 +1,25 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 /**
  * Created by carlhogo on 12/05/16.
  */
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerCollection;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyExample {
 
     public static void main(String[] args) throws Exception {
 
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-
-        Server server = new Server(2222);
         server.setHandler(context);
-
         ServletHolder servlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
-
         servlet.setInitOrder(0);
-
-        // Tells the Jersey Servlet which REST service/class to load.
-        servlet.setInitParameter(
-                "jersey.config.server.provider.classnames",
-                Resource.class.getCanonicalName());try {
-            server.start();
-            server.join();
-        } finally {
-            server.destroy();
-        }
-
+        servlet.setInitParameter("jersey.config.server.provider.classnames", Resource.class.getCanonicalName());
+        server.start();
+        server.join();
+        
     }
 }
